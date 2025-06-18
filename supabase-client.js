b60e4bf2bc80ea745f42b274a60b0f758a224d2f
nealpod <nealpodhuturi@gmail.com>
2025-06-18 19:19:15 -0400

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,11 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // These variables will be provided by Vercel's Environment Variables.
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://eeyfzopydsgusqkvhsrr.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVleWZ6b3B5ZHNndXNxa3Zoc3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNzA4MjEsImV4cCI6MjA2NTg0NjgyMX0.N4zNst3g0Ab4qE1Ykwqqx9e8ZXGeyldN_f3AihMdfXk";
 
+
+///import.meta.env.VITE_SUPABASE_URL 
+/// import.meta.env.VITE_SUPABASE_ANON_KEY
 // Create the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey);
