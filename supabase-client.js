681453fd5cdb81add49a8e4498d2212e42abce69
nealpod <nealpodhuturi@gmail.com>
2025-12-15 14:44:01 -0500

    Lock Supabase JS version to 2 in import
    Updated Supabase client import to lock version 2.

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// CHANGE IS HERE: We added "@2" to lock the version
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// These variables will be provided by Vercel's Environment Variables.
+// These variables are hardcoded here (which is fine for the Anon key)
 const supabaseUrl = "https://eeyfzopydsgusqkvhsrr.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVleWZ6b3B5ZHNndXNxa3Zoc3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNzA4MjEsImV4cCI6MjA2NTg0NjgyMX0.N4zNst3g0Ab4qE1Ykwqqx9e8ZXGeyldN_f3AihMdfXk";
 
